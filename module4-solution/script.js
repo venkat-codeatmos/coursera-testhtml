e5787309dffbbce7b50becd070c09d9e6a455534
Venk <venkat_1312@yahoo.com>
2016-10-08 16:01:18 +0530

    adding module 4 solution

---
+++ module4-solution/script.js
@@ -0,0 +1,16 @@
+
+(function () {
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0);
+  if (firstLetter === 'j' || firstLetter === 'J') {
+    byeSpeaker.speak(names[i])
+  } else {
+    helloSpeaker.speak(names[i])
+  }
+}
+
+})();
